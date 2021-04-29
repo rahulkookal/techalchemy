19ecef1d8b6cb95b7fb2e68467f6e8f2b9f1434d
rahulkookal <rahulkookal@qburst.com>
2021-04-29 13:03:08 +0530

    Finalisation

--- routes/users.js
+++ routes/users.js
@@ -2,87 +2,94 @@
 const express = require('express')
 const bcrypt = require('bcrypt')
 const router = express.Router()
-const { User } = require('./../models')
+const {
+    User
+} = require('./../models')
 const generateToken = require('../lib/generate-jwt-token')
 
 /* POST create new user */
 router.post('/register', async function(req, res, next) {
-	const {
-		first_name,
-		last_name,
-		email,
-		password
-	} = req.body
-	const hash = await bcrypt.hash(password, 10)
-	const user = User.build({
-		firstName: first_name,
-		lastName: last_name,
-		password: hash,
-		email: email
-	})
-	await user.save()
-	res.status(200).json(user)
+    try {
+        const {
+            first_name,
+            last_name,
+            email,
+            password
+        } = req.body
+        if (!email || !password) throw new Error('Email & Password required')
+        const hash = await bcrypt.hash(password, 10)
+        const user = User.build({
+            firstName: first_name,
+            lastName: last_name,
+            password: hash,
+            email: email
+        })
+        await user.save()
+        res.status(200).json(user)
+    } catch (error) {
+        res.status(400).json(error.message)
+    }
 });
 
 /* POST login user */
 router.post('/login', async function(req, res, next) {
-	try {
-		const {
-			email,
-			password
-		} = req.body
+    try {
+        const {
+            email,
+            password
+        } = req.body
 
-		if (!email || !password) throw new Error('Email & Password required')
+        if (!email || !password) throw new Error('Email & Password required')
 
-		const user = await User.findOne({
-			where: {
-				email: email
-			}
-		})
-		const match = await bcrypt.compare(password, user.password)
-		if (match) {
-			user.token = await generateToken({
-				email: user.email
-			})
-			// TODO: Token should be stored on cache or highly availabe systems like
-			// memcache or redis server
-			await user.save()
-			res.status(200).json({
-				token: user.token
-			})
-		} else {
-			throw new Error('Invalid Login')
-		}
+        const user = await User.findOne({
+            where: {
+                email: email
+            }
+        })
+        const match = await bcrypt.compare(password, user.password)
+        if (match) {
+            user.token = await generateToken({
+                email: user.email
+            })
+            // TODO: Token should be stored on cache or highly availabe systems like
+            // memcache or redis server
+            await user.save()
+            res.status(200).json({
+                token: user.token
+            })
+        } else {
+            throw new Error('Invalid Login')
+        }
 
-	} catch (error) {
-		res.status(400).json(error.message)
-	}
+    } catch (error) {
+        res.status(400).json(error.message)
+    }
 });
 
 /* POST create new user */
 router.post('/logout', async function(req, res, next) {
-	try {
-		const {
-			authorization
-		} = req.headers
+    try {
+        const {
+            authorization
+        } = req.headers
 
-		if (!authorization) throw new Error('You must send an Authorization header')
-		const [authType, token] = authorization.trim().split(' ')
+        if (!authorization) throw new Error('You must send an Authorization header')
+        const [authType, token] = authorization.trim().split(' ')
 
-		if (authType !== 'Bearer') throw new Error('Expected a Bearer token')
-		const user = await User.findOne({
-			where: {
-				token: token
-			}
-		})
+        if (authType !== 'Bearer') throw new Error('Expected a Bearer token')
+        const user = await User.findOne({
+            where: {
+                token: token
+            }
+        })
 
-		if (user == null) throw new Error('User already logged out')
-		user.token = null
-		await user.save()
-		res.status(200).json('User loged out')
-	} catch (error) {
-		res.status(401).json(error.message)
-	}
+        if (user == null) throw new Error('User already logged out')
+        user.token = null
+        await user.save()
+        res.status(200).json('User loged out')
+    } catch (error) {
+        res.status(400).json(error.message)
+    }
 });
 
 module.exports = router;
---
+++ test/index.js
@@ -0,0 +1,140 @@
+"use strict";
+const chai = require('chai');
+const chaiHttp = require('chai-http');
+const should = chai.should();
+const expect = chai.expect;
+chai.use(chaiHttp);
+const server = require('../app')
+
+describe('User', () => {
+    let token = ''
+    describe('/POST user', () => {
+        it('it should create new user', (done) => {
+            chai.request(server)
+                .post('/users/register')
+                .set('content-type', 'application/json')
+                .send({
+                    first_name: "rahul",
+                    last_name: "kodoth",
+                    email: "rahulkkodoth1@gmail.com",
+                    password: "admin123"
+                })
+                .end((err, res) => {
+                    (res.body).should.be.a('object')
+                    expect(res.body.email).to.equal('rahulkkodoth1@gmail.com')
+                    expect(res.body.firstName).to.equal('rahul')
+                    done();
+                });
+        })
+
+        it('it should throws email/passsword required error', (done) => {
+            chai.request(server)
+                .post('/users/register')
+                .set('content-type', 'application/json')
+                .send({
+                    first_name: "rahul",
+                    last_name: "kodoth",
+                    email: "rahulkkodoth1@gmail.com"
+                })
+                .end((err, res) => {
+                    (res.body).should.be.a('string')
+                    expect(res.body).to.equal("Email & Password required")
+                    done();
+                });
+        })
+
+        it('it should login user successfully', (done) => {
+            chai.request(server)
+                .post('/users/login')
+                .set('content-type', 'application/json')
+                .send({
+                    email: "rahulkkodoth1@gmail.com",
+                    password: "admin123"
+                })
+                .end((err, res) => {
+                    (res.body).should.be.a('object')
+                    token = res.body.token
+                    expect(res.body.token).to.not.equal(null)
+                    done();
+                });
+        })
+
+        it('it should throws email/passsword required error', (done) => {
+            chai.request(server)
+                .post('/users/login')
+                .set('content-type', 'application/json')
+                .send({
+                    email: "rahulkkodoth1@gmail.com",
+                    password: ""
+                })
+                .end((err, res) => {
+                    (res.body).should.be.a('string')
+                    expect(res.body).to.equal("Email & Password required")
+                    done();
+                });
+        })
+
+        it('it should throws invalid login', (done) => {
+            chai.request(server)
+                .post('/users/login')
+                .set('content-type', 'application/json')
+                .send({
+                    email: "rahulkkodoth1@gmail.com",
+                    password: "asaa"
+                })
+                .end((err, res) => {
+                    (res.body).should.be.a('string')
+                    expect(res.body).to.equal("Invalid Login")
+                    done();
+                });
+        });
+
+        it('it should throws Authorization header missing', (done) => {
+            chai.request(server)
+                .post('/users/logout')
+                .set('content-type', 'application/json')
+                .set('Authorization', '')
+                .end((err, res) => {
+                    (res.body).should.be.a('string')
+                    expect(res.body).to.equal("You must send an Authorization header")
+                    done();
+                });
+        })
+
+        it('it should throws bearer token missing', (done) => {
+            chai.request(server)
+                .post('/users/logout')
+                .set('content-type', 'application/json')
+                .set('Authorization', token)
+                .end((err, res) => {
+                    (res.body).should.be.a('string')
+                    expect(res.body).to.equal("Expected a Bearer token")
+                    done();
+                });
+        })        
+
+        it('it should throws user loged out', (done) => {
+            chai.request(server)
+                .post('/users/logout')
+                .set('content-type', 'application/json')
+                .set('Authorization', 'Bearer ' + token)
+                .end((err, res) => {
+                    (res.body).should.be.a('string')
+                    expect(res.body).to.equal("User loged out")
+                    done();
+                });
+        })
+
+        it('it should throws user already logged out', (done) => {
+          chai.request(server)
+              .post('/users/logout')
+              .set('content-type', 'application/json')
+              .set('Authorization', 'Bearer ' + token )
+              .end((err, res) => {
+                  (res.body).should.be.a('string')
+                  expect(res.body).to.equal("User already logged out")
+                  done();
+              });
+        })
+    })
+})
