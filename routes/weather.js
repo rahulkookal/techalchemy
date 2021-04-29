639bcffa6f602be2d96bbb909790cf8b4f19c2c3
rahulkookal <rahulkookal@qburst.com>
2021-04-29 10:11:55 +0530

    Code cleaning

--- routes/weather.js
+++ routes/weather.js
@@ -1,30 +1,28 @@
-const express = require('express');
-const router = express.Router();
-
-const getEverything = require('../lib/open-whether-api');
+const express = require('express')
+const router = express.Router()
+const getEverything = require('../lib/open-whether-api')
 
 /* GET news */
 router.get('/', async function(req, res, next) {
-  const weather = await getEverything()
+	const weather = await getEverything()
 
-  if (weather.cod == 200) {
-    let data = {
-      count: weather.cnt,
-      location: weather.city.name, 
-      data: weather.list.map((w) => { 
-        return { 
-          date: new Date(w.dt * 1000).toDateString(),
-          main: w.weather[0].main,
-          temp: w.main.temp
-        }
-      })
-    }
+	if (weather.cod == 200) {
+		const data = {
+			count: weather.cnt,
+			location: weather.city.name,
+			data: weather.list.map((w) => {
+				return {
+					date: new Date(w.dt * 1000).toDateString(),
+					main: w.weather[0].main,
+					temp: w.main.temp
+				}
+			})
+		}
 
-    res.send(data);
-  } else {
-    res.status(503).send(data)
-  }
-  
+		res.send(data);
+	} else {
+		res.status(503).send(data)
+	}
 });
 
 module.exports = router;
